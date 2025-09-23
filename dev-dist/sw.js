f46d28b7c806b68e219feb90a4ef83330b2721e2
Areek Ray <areek.ray@team.telstra.com>
2025-09-23 11:53:51 +0530

    updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-e328c5da'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.e0tqdpmia78"
+    "revision": "0.0nlgfmqfng8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
